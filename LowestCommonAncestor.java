8b4e68fef2950ac7d3afbd10ddebdbc5678b98c7
Thienbuu <51300328@hcmut.edu.vn>
2021-08-04 21:04:26 +0700

    Update LowestCommonAncestor.java

--- LowestCommonAncestor.java
+++ LowestCommonAncestor.java
@@ -9,6 +9,7 @@
  */
 
 class Solution {
+   
     //Iteration
      public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
         if (root == null) return null;
@@ -17,14 +18,14 @@ class Solution {
             p = q;
             q = temp;
         }
-        
+
         while (root != null) {
             if (root.val > p.val && root.val < q.val) return root;
             if (root.val == q.val || root.val == p.val) return root;
-            if (root.val > q.val) root = root.left;
-            else if (root.val < p.val) root = root.right; 
+            root = root.val < p.val ? root.right : root.left;
+
         } 
-        
+
         return null;
     }
     
@@ -43,5 +44,17 @@ class Solution {
 //         else if (root.val < p.val) root = root.right; 
         
 //         return lowestCommonAncestor(root, p, q);
+//     }
+    
+    //Without temp variable
+    //Can cause overflow
+//     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+//         if (root == null) return null;
+        
+//         while ((root.val - p.val)*(root.val - q.val) > 0) {
+//             root = p.val < root.val ? root.left : root.right;
+//         }
+        
+//         return root;
 //     }
 }
