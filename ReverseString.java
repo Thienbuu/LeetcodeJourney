9d9a7e4855352b421f21d5ddcd29cb32098ada6c
Thienbuu <51300328@hcmut.edu.vn>
2021-08-07 23:16:39 +0700

    Reverse String solution

---
+++ ReverseString.java
@@ -0,0 +1,11 @@
+class Solution {
+    public void reverseString(char[] s) {
+        int low = 0;
+        int high = s.length - 1;
+        while (high > low) {
+            char temp = s[high];
+            s[high--] = s[low];
+            s[low++] = temp;
+        }
+    }
+}
