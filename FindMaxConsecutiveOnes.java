8b3e239196a80b4c72eda637cfa29bc0505ad1d6
Thienbuu <51300328@hcmut.edu.vn>
2021-08-10 21:59:14 +0700

    Max Consecutive Ones solution

---
+++ FindMaxConsecutiveOnes.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int findMaxConsecutiveOnes(int[] nums) {
+        int max = 0;
+        int curr = 0;
+        for (int num : nums) {
+            if (num == 0) {
+                max = Math.max(curr,max); 
+                curr = 0;
+            }
+            else curr++;
+        }
+        return Math.max(max,curr);
+    }
+}
